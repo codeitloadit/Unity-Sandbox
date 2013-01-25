f140ac56e78294c88501a5782cb6ad3f671f668b
Brian Ellis <brian@codeitload.it>
2013-01-24 17:24:15 -0800

    Make It Rain

--- Assets/Scripts/script_bullet.js
+++ Assets/Scripts/script_bullet.js
@@ -4,20 +4,18 @@ var deathTime:float;
 var isDying:boolean;
 
 function Update() {
-	if (isDying)
-		deathTime -= Time.deltaTime;
+    if (isDying)
+        deathTime -= Time.deltaTime;
 
-	if (deathTime <= 0)
-		gameObject.collider.enabled = false;
+    if (deathTime <= 0)
+        gameObject.collider.enabled = false;
 
-	if (deathTime <= -1)
-		Destroy(gameObject);
-
-
+    if (deathTime <= -1)
+        Destroy(gameObject);
 }
 
 function OnCollisionEnter(other : Collision) {
-	if (other.gameObject.CompareTag('ground')) {
-		isDying = true;
-	}
+    if (other.gameObject.CompareTag('ground')) {
+        isDying = true;
+    }
 }
--- Assets/Scripts/script_player_shoot.js
+++ Assets/Scripts/script_player_shoot.js
@@ -13,16 +13,26 @@ function Update() {
     if (Input.GetKeyDown('right'))
         Shoot(transform.right);
 
-    if (Input.GetKeyDown('space')) {
+    if (Input.GetKey('space')) {
         Shoot(-transform.up);
         Shoot(transform.forward);
         Shoot(-transform.forward);
         Shoot(transform.right);
         Shoot(-transform.right);
     }
+
+    if (Input.GetKeyDown('x'))
+        MkaeItRain();
 }
 
 function Shoot(direction:Vector3) {
     var instanceBullet = Instantiate(prefabBullet, transform.position+Vector3.up - direction, Quaternion.identity);
-    instanceBullet.rigidbody.AddForce((-direction+(Vector3.up)) * shootForce);
+    instanceBullet.rigidbody.AddForce((-direction) * shootForce);
+}
+
+function MkaeItRain() {
+    for (var i = 0; i < 100; i++) {
+        var position = Vector3(Random.Range(-14, 14), Random.Range(5, 9), Random.Range(-14, 14));
+        Instantiate(prefabBullet, position, Quaternion.identity);
+    };
 }
